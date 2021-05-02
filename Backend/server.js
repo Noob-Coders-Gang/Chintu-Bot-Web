46e18fb79a3b911ac829ff6d4498be49c12112a3
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-05-02 14:41:18 +0530

    mongodb connection setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors')
+const mongoose = require('mongoose');
+
 
 require('dotenv').config();
 
@@ -8,6 +10,19 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connected :) ")
+})
+
 
 app.listen(port, () => {
     console.log(`Server is up , port :${port}`)
